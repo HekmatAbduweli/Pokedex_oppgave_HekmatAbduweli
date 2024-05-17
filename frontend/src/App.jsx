d7e974364e3fb4170b451665d35c307df560c09e
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2024-05-17 22:00:48 +0200

    prøver å hente types fra api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Teams from "./components/Teams";
+import Type from "./components/Type";
+import Searchresult from "./components/Searchresult";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +19,10 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/teams" element={<Teams/>}></Route>
+            <Route path="/teams" element={<Teams />}></Route>
+            <Route path="/:type" element={<Type />}></Route>
+            <Route path="/searchresult/:pokemon" element={<Searchresult />}></Route>
+            <Route path="/pokemons/:pokemon" element={<Pokemon />}></Route>
           </Routes>
         </main>
       </div>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <h1> This is home page</h1>;
+  const [pokemonTypes, setPokemonTypes] = useState([]);
+
+  useEffect(() => {
+    const pokemonFetch = async () => {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/type");
+        const data = await response.json();
+        setPokemonTypes(data.results);
+        console.log(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    pokemonFetch();
+  }, []);
+
+  return (
+    <>
+      <h1>Main Pokemons</h1>
+      <h2>Types</h2>
+      <ul>
+        {pokemonTypes.map((type) => (
+          <li key={type.name}>{type.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
