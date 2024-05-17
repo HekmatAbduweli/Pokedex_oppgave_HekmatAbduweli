79a19b2d8f40d9b974711ee42fda55bd270e60e5
HekmatAbduweli <143352767+HekmatAbduweli@users.noreply.github.com>
2024-05-17 02:05:44 +0200

    tester router og components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import Teams from "./components/Teams";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>hello</h1>
+      <div>
+        <header>
+          <a href="/">UNI POKEDEX</a>
+          <a href="/teams">Teams</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/teams" element={<Teams/>}></Route>
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/Teams.jsx
@@ -0,0 +1,3 @@
+export default function Teams() {
+  return <h1>This is Team page</h1>;
+}
